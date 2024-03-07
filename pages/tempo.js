68b07b4e35012811b529888a72297786fb0b1d10
Saky0 <daviteremattos@gmail.com>
2024-03-07 14:09:57 -0300

    Added revalidate

--- pages/tempo.js
+++ pages/tempo.js
@@ -17,7 +17,10 @@ export function getStaticProps() {
     const staticDateString = staticDate.toGMTString();  
 
     return {
-        props: {staticDateString}
+        props: {
+            staticDateString
+        },
+        revalidate: 1
     }
 }
 
