0ccdf27ab2256616f23c605e349998f6c364533b
Saky0 <daviteremattos@gmail.com>
2024-03-07 14:01:33 -0300

    New page Tempo

---
+++ pages/tempo.js
@@ -0,0 +1,24 @@
+function Tempo(props) {
+    console.log('> Passando pelo Frontend;')
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();  
+
+    return (
+        <div>
+            <div>{dynamicDateString} (Dinamico)</div>
+            <div>{props.staticDateString} (Static)</div>
+        </div>
+    ) 
+}
+
+export function getStaticProps() {
+    console.log('> Passando pelo getStaticProps();')
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();  
+
+    return {
+        props: {staticDateString}
+    }
+}
+
+export default Tempo
