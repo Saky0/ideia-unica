4d48e2bec34cdd208be17a6bd4dc61520f9e64d5
Saky0 <daviteremattos@gmail.com>
2024-03-07 12:02:29 -0300

    First Page

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Contador/>
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1)
+    }
+
+    function SubtrairContador() {
+        setContador(contador - 1)
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+            <button onClick={SubtrairContador}>Subtrair</button>
+        </div>
+    )
+}
+
+function CapsLock(props) {
+    const textoInserido = props.children
+    const textoEmCapsLock = textoInserido.toUpperCase()
+
+    return (
+        <div>{textoEmCapsLock}</div>
+    )
+}
+
+export default Home
