8fccd903edf22663705e8610f7b9eaaa74f5f078
Saky0 <daviteremattos@gmail.com>
2024-03-07 14:50:07 -0300

    added env

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,14 @@
-function tempo(request, response) {
+async function tempo(request, response) {
+    const apiSecret = process.env.POKE_API_SECRET
     const dynamicDate = new Date();
 
+    const pokemonResponse = await fetch(`${apiSecret}`);
+    const pokemonResponseJson = await pokemonResponse.json();
+    const pokemonName = pokemonResponseJson.results[0].name
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        pokemonName: pokemonName
     })
 }
 
