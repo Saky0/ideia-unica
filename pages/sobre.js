e54a38fadd2ec7a7b02f7dc8355695ae783d3e51
Saky0 <daviteremattos@gmail.com>
2024-03-07 13:28:26 -0300

    New page About

---
+++ pages/sobre.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+function Sobre() {
+    return (
+        <div>
+            <h1>Sobre</h1>
+
+            <Link href="/">
+                <div>Va para a página Home</div>
+            </Link>   
+        </div>
+    )
+}
+
+export default Sobre
